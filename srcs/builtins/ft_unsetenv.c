a5e80da8f7087e84f66ee125d709c689e3d7257f
npineau <nofigh@hotmail.fr>
2014-03-03 15:37:42 +0100

    modification de ft_unsetenv pour les listes.

--- srcs/builtins/ft_unsetenv.c
+++ srcs/builtins/ft_unsetenv.c
@@ -6,55 +6,49 @@
 /*   By: cheron <cheron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/24 16:41:56 by cheron            #+#    #+#             */
-/*   Updated: 2014/02/26 15:59:24 by cheron           ###   ########.fr       */
+/*   Updated: 2014/03/03 15:36:00 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <libft.h>
 #include "../ft_minishell.h"
 
-static int		ft_getnewsize(t_dat *dat, char **cmd_split);
-
-void			ft_unsetenv(t_dat *dat, char **cmd_split)
+static t_env	*get_pre(t_env *env, char *src)
 {
-	int		size;
-	char	**new;
-	int		i;
-	int		len;
-	int		j;
-
-	i = 0;
-	j = 0;
-	len = ft_strlen(cmd_split[1]);
-	size = ft_getnewsize(dat, cmd_split);
-	new = malloc(sizeof(char **) * size + 1);
-	while (dat->env[i])
+	if (env->next)
 	{
-		if (cmd_split[1] && ft_strncmp(cmd_split[1], dat->env[i], len))
-		{
-			new[j] = ft_strdup(dat->env[i]);
-			j++;
-		}
-		i++;
+		if (ft_strequ(env->next->var, src))
+			return (env);
+		else
+			return (get_prev(env->next, src));
 	}
-	dat->env = new;
+	return (NULL);
 }
 
-static int		ft_getnewsize(t_dat *dat, char **cmd_split)
+void			ft_unsetenv(t_dat *dat, char **cmd)
 {
-	int		size;
-	int		i;
-	int	len;
+	t_env		*tmp;
+	t_env		*swap;
 
-	i = 0;
-	size = 0;
-	len = ft_strlen(cmd_split[1]);
-	while (dat->env[i])
+	if (!cmd[1])
 	{
-		if (cmd_split[1] && ft_strncmp(cmd_split[1], dat->env[i], len))
-			size += ft_strlen(dat->env[i]);
-		i++;
+		ft_putendl("Usage: unsetenv VAR");
+		return ;
 	}
-	return (size);
+	if (ft_strequ(dat->env->var, cmd[1]))
+	{
+		tmp = dat->env;
+		dat->env = dat->env->next;
+		//////////////////////////////////free dis shiet
+		return ;
+	}
+	else
+	{
+		swap = get_pre(dat->env, cmd[1]);
+		if (!swap)
+			return ;
+		tmp = swap->next;
+		swap->next = tmp->next;
+		//////////////////////////////////free dis shiet
+	}
 }
