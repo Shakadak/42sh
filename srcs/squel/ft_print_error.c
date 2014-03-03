96cfe5e4c23af978bb8dd36e942b5fd0e8cd1ad9
npineau <nofigh@hotmail.fr>
2014-03-03 19:36:48 +0100

    FIXEED FREAKING HUUUUUUUGE BUGgit add srcs/squel/ft_print_error.cgit add srcs/squel/ft_print_error.caddgit add srcs/squel/ft_print_error.c WE'RE ALL SAVED

--- srcs/squel/ft_print_error.c
+++ srcs/squel/ft_print_error.c
@@ -6,7 +6,7 @@
 /*   By: cheron <cheron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/29 15:00:40 by cheron            #+#    #+#             */
-/*   Updated: 2014/02/26 16:25:40 by cheron           ###   ########.fr       */
+/*   Updated: 2014/03/03 19:35:32 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_print_error(char *cmd, int code)
 {
 	if (code == 1)
 	{
-		ft_putstr_fd("ft_minishell: command not found: ", 2);
+		ft_putstr_fd("42sh: command not found: ", 2);
 		ft_putendl_fd(cmd, 2);
 	}
 }
