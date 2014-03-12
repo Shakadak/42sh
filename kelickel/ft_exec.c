32adbca815aefa45a1b6c6fc2b1916680e9f2655
Kevin LICKEL <kelickel@e1r1p7.42.fr>
2014-03-12 17:25:13 +0100

    mon parseur encore beuger

---
+++ kelickel/ft_exec.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exec.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kelickel <kelickel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/03 10:22:08 by kelickel          #+#    #+#             */
+/*   Updated: 2014/03/12 14:21:28 by kelickel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_sh.h"
+
+int				ft_exec(char **env, char **arg)
+{
+	if (ft_builtins(arg) == 0)
+		return (ft_execenv(env, arg));
+	else
+		return (1);
+}
+
+int				ft_execenv(char **env, char **arg)
+{
+	char		*str;
+	int			a;
+	char		*x;
+
+	check_exec = 1;
+	execve(arg[0], arg, env);
+	a = 0;
+	while (a < 8)
+	{
+			str = ft_path();
+			if (str == 0)
+			{
+			ft_putstr("$: Command not found: ");
+			ft_putstr(arg[0]);
+			ft_putchar('\n');
+			check_exec = 0;
+			exit(-1);
+		}
+		x = ft_strnew(ft_strlen(str) + ft_strlen(arg[0]) + 2);
+		ft_strcat(x, str);
+		ft_strcat(x, "/");
+		ft_strcat(x, arg[0]);
+		execve(x, arg, env);
+		a = a + 1;
+	}
+	exit (-1);
+}
---
+++ kelickel/ft_system.c
@@ -0,0 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_system.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kelickel <kelickel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/03 10:53:16 by kelickel          #+#    #+#             */
+/*   Updated: 2014/03/12 14:30:11 by kelickel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_sh.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int	ft_system(char *str)
+{
+	int			k;
+	extern char	**environ;
+	char		**arg;
+
+	arg = ft_strsplit(str, ' ');
+	if (ft_builtins(arg) == 1)
+		return (1);
+	k = fork();
+	if (k == 0)
+		ft_exec(environ, arg);
+	else if (k > 0)
+	{
+		wait(&k);
+		if (k == 0)
+			return (1);
+		else
+			return (-1);
+	}
+	else
+		exit(-1);
+}
+
+int	ft_pipe(char *str)
+{
+	int			fd[2];
+	int			k;
+	extern char	**environ;
+	char		**arg;
+
+	arg = ft_strsplit(str, ' ');
+	pipe(fd);
+	k = fork();
+	if (k == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], 1);
+		ft_exec(environ, arg);
+		close(fd[1]);
+	}
+	else if (k > 0)
+	{
+		close(fd[1]);
+		dup2(fd[0], 0);
+		wait(&k);
+		close(fd[0]);
+		if (k == 0)
+			return (1);
+		else
+			return (-1);
+	}
+}
+
+int	ft_in(char *str, char *all, int *i, char **av)
+{
+	int	fd;
+	char	*tmp;
+	int	a;
+
+	a = 0;
+	*i = *i + 1;
+	tmp = malloc(sizeof(char *) * ft_strlen(all));
+	while (all[*i] == ' ')
+		*i = *i + 1;
+	while (all[*i] != ' ')
+	{
+		tmp[a++] = all[*i];
+		*i = *i + 1;
+	}
+	tmp[*i] = 0;
+	fd = open(tmp, O_RDONLY);
+	if (fd == -1)
+		write(2, "Can't open file\n", 17);
+	else
+		dup2(fd, 0);
+}
+
+int	ft_out(char *str, char *all, int *i, char **av)
+{
+	int		fd;
+	char	*tmp;
+	int		a;
+	int		oldfd;
+	int		param;
+
+	oldfd = dup(1);
+	a = 0;
+	*i = *i + 1;
+	if (all[*i] == '>')
+	{
+		param = 1;
+		*i = *i + 1;
+	}
+	else
+		param = 0;
+	while (all[*i] == ' ' || all[*i] == '\t')
+		*i = *i + 1;
+	tmp = malloc(sizeof(char *) * ft_strlen(all));
+	while (all[*i] != ' ' && all[*i] != '\0')
+	{
+		tmp[a++] = all[*i];
+		*i = *i + 1;
+	}
+	tmp[*i] = 0;
+	if (param == 0)
+		fd = open(tmp, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	else
+		fd = open(tmp, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd == -1)
+		write(2, "Can't open the file\n", 20);
+	else
+	{
+		dup2(fd, 1);
+		ft_system(str);
+		dup2(oldfd, 1);
+		close(fd);
+		close(oldfd);
+	}
+}
